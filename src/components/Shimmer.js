84657384b93341ba5b52a171baf36e43c78afc1e
Neelakantam-Jami <neelakantamjami24@gmail.com>
2025-05-18 09:43:20 +0530

    final commit

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,26 +1,27 @@
 const Shimmer = () => {
     return (
-      <div className="shimmer-container">
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
-        <div className="shimmer-card"></div>
+      <div className="w-10/12 flex flex-wrap mx-auto justify-evenly mt-30">
+        <div className="bg-gray-100 w-80 h-80 my-4"></div>
+        <div className="bg-gray-100 w-80 h-80 my-4"></div>
+        <div className="bg-gray-100 w-80 h-80 my-4"></div>
+        <div className="bg-gray-100 w-80 h-80 my-4"></div>
+        <div className="bg-gray-100 w-80 h-80 my-4"></div>
+        <div className="bg-gray-100 w-80 h-80 my-4"></div>
+        <div className="bg-gray-100 w-80 h-80 my-4"></div>
+        <div className="bg-gray-100 w-80 h-80 my-4"></div>
+        <div className="bg-gray-100 w-80 h-80 my-4"></div>
+        <div className="bg-gray-100 w-80 h-80 my-4"></div>
+        <div className="bg-gray-100 w-80 h-80 my-4"></div>
+        <div className="bg-gray-100 w-80 h-80 my-4"></div>
+        <div className="bg-gray-100 w-80 h-80 my-4"></div>
+        <div className="bg-gray-100 w-80 h-80 my-4"></div>
+        <div className="bg-gray-100 w-80 h-80 my-4"></div>
+        <div className="bg-gray-100 w-80 h-80 my-4"></div>
+        <div className="bg-gray-100 w-80 h-80 my-4"></div>
+        <div className="bg-gray-100 w-80 h-80 my-4"></div>
+        <div className="bg-gray-100 w-80 h-80 my-4"></div>
+        <div className="bg-gray-100 w-80 h-80 my-4"></div>
+        <div className="bg-gray-100 w-80 h-80 my-4"></div>
       </div>
     );
 }
